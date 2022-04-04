e1d966d837770ea93027a4ab95bff6dd9f983a74
nikki <6318495+echrs@users.noreply.github.com>
2022-04-04 21:35:19 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require('path');
 
 require("dotenv").config();
 
@@ -9,6 +10,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "client", "build")));
 
 const uri = process.env.ATLAS_URI;
 mongoose
@@ -18,6 +20,11 @@ mongoose
 
 app.use("/ingredient", require("./routes/ingredient"));
 app.use("/user", require("./routes/user"));
+
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
+})
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
