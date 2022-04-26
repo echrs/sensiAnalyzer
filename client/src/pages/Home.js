b69d0dee5f012be4ee84080b1bb09175d435ec7a
nikki <6318495+echrs@users.noreply.github.com>
2022-04-26 20:54:04 +0200

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -17,22 +17,28 @@ export default function Home() {
   const [openModal, setOpenModal] = React.useState(false);
   const handleOpenModal = () => setOpenModal(true);
   const handleCloseModal = () => setOpenModal(false);
-  const [txt, setTxt] = React.useState('');
-
+  const [txt, setTxt] = React.useState("");
+  const [flag, setFlag] = React.useState(false);
   const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
     width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
+    borderRadius: "4px",
+    bgcolor: "background.paper",
     boxShadow: 24,
     p: 4,
   };
 
-  const [text, setText] = React.useState("");
+  const [inputText, setInputText] = React.useState("");
 
+  const initialFiltersState = [
+    { name: "Fragrances", checked: false, ingredients: fragrances, found: false },
+    { name: "Alcohols", checked: false, ingredients: alcohols, found: false },
+    { name: "Parabens", checked: false, ingredients: parabens, found: false },
+    { name: "Surfactants", checked: false, ingredients: surfactants, found: false },
+  ];
   const [filters, setFilters] = React.useState([
     { name: "Fragrances", checked: false, ingredients: fragrances, found: false },
     { name: "Alcohols", checked: false, ingredients: alcohols, found: false },
@@ -41,28 +47,69 @@ export default function Home() {
   ]);
 
   const analyze = () => {
-    if (text) {
-      let arr = text.ingredientsText.split(/[,\\/|]/);
+    if (inputText) {
+      let arr = inputText.split(/[,\\/|]/);
       arr = arr.map((a) => a.replace(/\./g, ""));
       arr = arr.map((a) => a.trim());
       arr = arr.map((a) => a.toUpperCase());
       let updatedFilters = filters.map((filter) => {
-            return { ...filter, found: arr.some(a => filter.ingredients.includes(a)) };
-      })
+        return {
+          ...filter,
+          found: arr.some((a) => filter.ingredients.includes(a)),
+        };
+      });
       setFilters(updatedFilters);
-      let flagged = filters.some((filter) => filter.checked && filter.found);
-      if(flagged) {
-        setTxt("Oops"); 
-        handleOpenModal();}
-      else {
-        setTxt("Yay"); 
+      let flagged = updatedFilters.filter(
+        (filter) => filter.checked && filter.found
+      );
+      if (flagged.length > 0) {
+        switch (flagged.length) {
+          case 1:
+            setTxt(
+              `Oh no! Your product contains ${flagged[0].name.toLowerCase()}.`
+            );
+            break;
+          case 2:
+            setTxt(
+              `Oh no! Your product contains ${flagged[0].name.toLowerCase()} and ${flagged[1].name.toLowerCase()}.`
+            );
+            break;
+          case 3:
+            setTxt(
+              `Oh no! Your product contains ${flagged[0].name.toLowerCase()}, ${flagged[1].name.toLowerCase()} and ${flagged[2].name.toLowerCase()}.`
+            );
+            break;
+          case 4:
+            setTxt(
+              `Oh no! Your product contains ${flagged[0].name.toLowerCase()}, ${flagged[1].name.toLowerCase()}, ${flagged[2].name.toLowerCase()} and ${flagged[3].name.toLowerCase()}.`
+            );
+            break;
+          default:
+            setTxt("Oops!");
+            break;
+        }
+        setFlag(true);
+        handleOpenModal();
+      } else {
+        setFlag(false);
+        setTxt("Good news! This product should be good for you.");
         handleOpenModal();
       }
     }
   };
 
+  const saveProduct = () => {
+    handleCloseModal();
+  };
+
+  const resetAll = () => {
+    setInputText("");
+    setFilters(initialFiltersState);
+    handleCloseModal();
+  }
+
   const handleChange = (e) => {
-    setText({ ...text, [e.target.name]: e.target.value });
+    setInputText(e.target.value);
   };
 
   const handleCheckboxChange = (position) => {
@@ -78,81 +125,97 @@ export default function Home() {
 
   return (
     <>
-    <Grid
-      spacing={1}
-      container
-      direction="column"
-      justifyContent="center"
-      alignItems="center"
-      style={{ minHeight: "calc(100vh - 64px)" }}
-    >
-      <Grid item>
-        <Typography variant="h2">
-          Sensitive skin? We are here to help.
-        </Typography>
+      <Grid
+        spacing={1}
+        container
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        style={{ minHeight: "calc(100vh - 64px)" }}
+      >
+        <Grid item>
+          <Typography variant="h2">
+            Sensitive skin? We are here to help.
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Typography variant="h2">
+            Select what you *don’t* want in your product.
+          </Typography>
+        </Grid>
+        <Grid item>
+          <FormGroup aria-label="position" row>
+            {filters.map(({ name }, index) => {
+              return (
+                <FormControlLabel
+                  key={index}
+                  value={name}
+                  control={
+                    <Checkbox
+                      checked={filters[index].checked}
+                      onChange={() => handleCheckboxChange(index)}
+                    />
+                  }
+                  label={name}
+                />
+              );
+            })}
+          </FormGroup>
+        </Grid>
+        <Grid item>
+          <FormControl sx={{ width: "90ch" }}>
+            <OutlinedInput
+              name="ingredientsInputText"
+              style={{ background: "#ffffff" }}
+              multiline
+              minRows={10}
+              placeholder="Paste your ingredients here..."
+              value={inputText}
+              onChange={handleChange}
+            />
+          </FormControl>
+        </Grid>
+        <Grid item>
+          <Button
+            type="submit"
+            onClick={analyze}
+            disableElevation
+            variant="contained"
+          >
+            Go!
+          </Button>
+        </Grid>
       </Grid>
-      <Grid item>
-        <Typography variant="h2">
-          Select what you *don’t* want in your product.
-        </Typography>
-      </Grid>
-      <Grid item>
-        <FormGroup aria-label="position" row>
-          {filters.map(({ name }, index) => {
-            return (
-              <FormControlLabel
-                key={index}
-                value={name}
-                control={
-                  <Checkbox
-                    checked={filters[index].checked}
-                    onChange={() => handleCheckboxChange(index)}
-                  />
-                }
-                label={name}
-              />
-            );
-          })}
-        </FormGroup>
-      </Grid>
-      <Grid item>
-        <FormControl sx={{ width: "90ch" }}>
-          <OutlinedInput
-            name="ingredientsText"
-            style={{ background: "#ffffff" }}
-            multiline
-            minRows={10}
-            placeholder="Paste your ingredients here..."
-            onChange={handleChange}
-          />
-        </FormControl>
-      </Grid>
-      <Grid item>
-        <Button
-          type="submit"
-          onClick={analyze}
-          disableElevation
-          variant="contained"
-        >
-          Go!
-        </Button>
-      </Grid>
-    </Grid>
-    <Modal
-    open={openModal}
-    onClose={handleCloseModal}
-    aria-labelledby="modal-modal-title"
-    aria-describedby="modal-modal-description"
-  >
-    <Box sx={style}>
-      <Typography id="modal-modal-title" variant="h6" component="h2">
-        {txt}
-      </Typography>
-      <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-      </Typography>
-    </Box>
-  </Modal>
-  </>
+      <Modal
+        open={openModal}
+        onClose={handleCloseModal}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            {txt}
+          </Typography>
+          {flag && (
+            <Button
+              onClick={resetAll}
+              disableElevation
+              variant="contained"
+            >
+              TRY AGAIN?
+            </Button>
+          )}
+          {!flag && (
+            <Button
+              onClick={saveProduct}
+              disableElevation
+              variant="contained"
+            >
+              SAVE
+            </Button>
+          )}
+        </Box>
+      </Modal>
+    </>
   );
 }
